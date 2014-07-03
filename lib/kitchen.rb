6852100407351ca9f61249d8c1e900f4c0178f71
Sam Joseph <tansaku@gmail.com>
2014-07-03 13:30:58 +0100

    gosh this really stinks

--- lib/kitchen.rb
+++ lib/kitchen.rb
@@ -1,11 +1,9 @@
-def do_something(c)
-  c.sort do |a,b| 
-    a[:o]+a[:n] <=> b[:o]+b[:n]
+def do_something(c, d2=[],k5={},f=true)
+  c.sort do |a,b| a[:o]+a[:n] <=> b[:o]+b[:n]
   end.select do |i|
-    i[:s] == :c
-  end.select do |i|
-    i[:l] == :c
-  end.each do |i|
+    i[:s] == :c end.select do |i|
+      i[:l] == :c
+    end.each do |i|
     puts "#{i[:o]}'s #{i[:n]}"
-  end
+    end
 end
